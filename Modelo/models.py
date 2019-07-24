63fcc6c4451bc6abb630214d8dc54b03641f86cc
MacaEsru <163170@ids.upchiapas.edu.mx>
2019-07-24 09:10:30 -0500

    add nuevos cambios

--- Modelo/models.py
+++ Modelo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Registro(models.Model):
-    name_registro = models.CharField(max_length=254, null=False)
-    delete_registro = models.BooleanField(default=False)
-    create_registro = models.DateTimeField(default=timezone.now)
+    idAlumno = models.ForeignKey(Alumno, null=True, blank=True, on_delete=models.CASCADE)
+    delete = models.BooleanField(default=False)
+    create = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
@@ -13,13 +13,14 @@ class Registro(models.Model):
         db_table = 'Registro'
 
 class Alumno(models.Model):
-    rfid_alumno = models.OneToOneField(Registro, null=True, blank=True, on_delete=models.CASCADE)
-    name_alumno = models.CharField(max_length=254, null=False)
-    matricula_alumno = models.IntegerField(null=False)
-    motherslast_alumno = models.CharField(max_length=254, null=False)
-    fatherslast_alumno = models.CharField(max_length=254, null=False)
-    delete_alumno = models.BooleanField(default=False)
-    create_alumno = models.DateTimeField(default=timezone.now)
+    rfidAlumno = models.CharField(max_length=254, null=False)
+    nameAlumno = models.CharField(max_length=254, null=False)
+    apellidoMaterno = models.CharField(max_length=254, null=False)
+    apellidoPaterno = models.CharField(max_length=254, null=False)
+    matriculaAlumno = models.IntegerField(null=False)
+    carreraAlumno = models.CharField(max_length=254, null=False)
+    delete = models.BooleanField(default=False)
+    create = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
