15efa34b26b3f3063de07df5f130aa8465a7134a
MacaEsru <163170@ids.upchiapas.edu.mx>
2019-07-24 09:22:53 -0500

    models

--- Modelo/models.py
+++ Modelo/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-class Registro(models.Model):
-    id_alumno = models.ForeignKey(Alumno, null=True, blank=True, on_delete=models.CASCADE)
-    delete = models.BooleanField(default=False)
-    create = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        db_table = 'Registro'
-
 class Alumno(models.Model):
     rfid_alumno = models.CharField(max_length=254, null=False)
     name_alumno = models.CharField(max_length=254, null=False)
@@ -27,3 +16,15 @@ class Alumno(models.Model):
     
     class Meta:
         db_table = 'Alumno'
+
+class Registro(models.Model):
+    id_alumno = models.ForeignKey(Alumno, null=True, blank=True, on_delete=models.CASCADE)
+    delete = models.BooleanField(default=False)
+    create = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'Registro'
+
